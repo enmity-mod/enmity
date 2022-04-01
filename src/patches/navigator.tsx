082b304bc1cff3675d70ccf0f53a8db2f7e01a6b
Zoey <zoey@panties.moe>
2022-04-01 16:01:13 -0400

    remove navigator patches

--- src/patches/navigator.tsx
+++
@@ -1,8 +0,0 @@
-import { getModule, getModules } from '../utils/modules';
-import { create } from '../utils/patcher';
-import { reloadDiscord } from '../api/native';
-
-export function patchNavigator(): void {
-  const Patcher = create('NaviagtorPatch');
-  const Navigator = getModule(m => m.LeftButtonWithChevron);
-}
