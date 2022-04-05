184acfbfc30a8547448e57ce6185b9a1f14a8b6e
Zoey <zoey@panties.moe>
2022-04-05 13:48:53 -0400

    create hash after build

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig, Plugin, RenderedChunk } from "rollup";
 import replace from '@rollup/plugin-replace';
 import esbuild from "rollup-plugin-esbuild";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
+import crypto from "crypto";
+import fs from "fs";
 import { execSync } from "child_process";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 const revision = execSync('git rev-parse --short HEAD').toString().trim()
 
@@ -21,6 +23,22 @@ export default defineConfig({
     replace({
       preventAssignment: true,
       '__VERSION__': revision
-    })
+    }),
+    createHash()
   ]
 });
+
+function createHash(options = {}): Plugin {
+  return {
+    name: 'plugin-info',
+    buildEnd: (err) => {
+      const buffer = fs.readFileSync('dist/Enmity.js');
+      const hash = crypto.createHash('sha256');
+      hash.update(buffer);
+
+      const hex = hash.digest('hex');
+
+      fs.writeFileSync('dist/hash', hex);
+    }
+  }
+};
