5803020e8e36661014a713edb2858a3711eefac7
Zoey <zoey@panties.moe>
2022-04-07 09:02:25 -0400

    lint fixes

--- src/api/clyde.ts
+++ src/api/clyde.ts
@@ -4,19 +4,19 @@ const Messages = getModule(m => m.default?.receiveMessage);
 const BotMessages = getModule(m => m.createBotMessage);
 const Images = getModule(m => m.default?.BOT_AVATARS);
 
-Images.default.BOT_AVATARS.ENMITY = "https://github.com/enmity-mod.png";
+Images.default.BOT_AVATARS.ENMITY = 'https://github.com/enmity-mod.png';
 
 export function sendReply(channelID: string, content: (string | object), username?: string, avatarURL?: string): void {
-  const msg = BotMessages.createBotMessage(channelID, "");
+  const msg = BotMessages.createBotMessage(channelID, '');
 
-  msg.author.username = username ?? "Enmity";
-  msg.author.avatar = avatarURL ? username : "ENMITY"; 
-  
+  msg.author.username = username ?? 'Enmity';
+  msg.author.avatar = avatarURL ? username : 'ENMITY';
+
   if (avatarURL) {
     Images.default.BOT_AVATARS[username] = avatarURL;
   }
-  
-  if (typeof content === "string") {
+
+  if (typeof content === 'string') {
     msg.content = content;
   } else {
     Object.assign(msg, content);
