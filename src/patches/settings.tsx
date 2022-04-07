42fc2983e06fadfc0f6b4f0ddbc045663d80e103
eternal <eternal.git@gmail.com>
2022-04-07 05:38:22 +0100

    clean up settings patch

--- src/patches/settings.tsx
+++ src/patches/settings.tsx
@@ -1,94 +1,100 @@
-import { getModule, getModules } from '../utils/modules';
-import { PluginPage } from '../screens/plugins';
-import { create } from '../utils/patcher';
-import { reloadDiscord } from '../api/native';
-import { showDialog } from '../api/dialog';
+import { FormArrow, FormDivider, FormLabel, FormRow, FormSection, React } from "../api/react";
+import { PluginPage } from "../screens/plugins";
+import { ThemePage } from "../screens/themes";
+import { reloadDiscord } from "../api/native";
+import { getModule } from "../utils/modules";
+import { showDialog } from "../api/dialog";
+import { create } from "../utils/patcher";
 
-import {
-  Button,
-  Form,
-  FormArrow,
-  FormDivider,
-  FormLabel,
-  FormRow,
-  FormSection,
-  FormText,
-  React,
-  StatusBar,
-  Text,
-  View,
-  useState,
-} from '../api/react';
-
-const navigationModule = getModule(m => m.default?.pushLazy);
+const Navigation = getModule(m => m.default?.pushLazy);
 
 export function patchSettings(): void {
-  const Patcher = create('SettingsPatch');
+   const Patcher = create("enmity-settings");
 
-  let UserSettingsOverview;
-  const Settings = getModule(m => m.default?.name === 'UserSettingsOverviewWrapper');
-  const unpatch = Patcher.after(Settings, 'default', (_, args, res) => {
-    if (UserSettingsOverview !== undefined) {
-      return;
-    }
+   let UserSettingsOverview;
+   const Settings = getModule(m => m.default?.name === "UserSettingsOverviewWrapper");
+   const unpatch = Patcher.after(Settings, "default", (_, args, res) => {
+      if (UserSettingsOverview !== void 0) {
+         return;
+      }
 
-    unpatch();
+      unpatch();
 
-    UserSettingsOverview = res.type;
-
-    Patcher.after(UserSettingsOverview.prototype, 'render', (_, args, res) => {
-      const children = res.props.children;
-      const Messages = getModule(x => x.default?.Messages).default.Messages;
-      const nitroIndex = children.findIndex(x => x.props.title === Messages['PREMIUM_SETTINGS']);
-      const nitro = children[nitroIndex];
+      UserSettingsOverview = res.type;
 
       const { openURL } = getModule(m => m.handleSupportedURL);
-      const { version } = window['enmity'];
+      const Messages = getModule(x => x.default?.Messages).default.Messages;
 
-      const enmitySection =
-        <><FormSection title="Enmity">
-          <FormRow label="Enmity" trailing={<FormLabel text={version}/>} onPress={(): void => {
-            openURL(`https://github.com/enmity-mod/enmity/commit/${version}`);
-          }}></FormRow>
-          <FormDivider />
-          <FormRow label="Plugins" trailing={<FormArrow/>} onPress={(): void => {
-            navigationModule.default.push(PluginPage, {});
-          }}></FormRow>
-          <FormDivider />
-          <FormRow label="Themes" trailing={<FormArrow/>} onPress={(): void => {
-            showDialog({
-              title: 'Coming soon',
-              confirmText: 'Okay',
-            });
-          }}></FormRow>
-          <FormDivider />
-          <FormRow label={<FormLabel style={{ color: '#FFC0CB' }} text={'Reload Discord'}/>} arrowShown={true} onPress={(): void => {
-            showDialog({
-              title: 'Are you sure you want to reload Discord?',
-              confirmText: 'Yes',
-              cancelText: 'No',
-              onConfirm() {
-                reloadDiscord();
-              },
-            });
-          }}></FormRow>
-        </FormSection>
-        <FormSection>
-          <FormRow label={<FormLabel text={'Discord'}/>} trailing={<FormArrow/>} onPress={(): void => {
-            openURL('https://discord.gg/rMdzhWUaGT');
-          }}></FormRow>
-          <FormDivider />
-          <FormRow label={<FormLabel text={'GitHub'}/>} trailing={<FormArrow/>} onPress={(): void => {
-            openURL('https://github.com/enmity-mod/enmity');
-          }}></FormRow>
-          <FormDivider />
-          <FormRow label={<FormLabel text={'Twitter'}/>} trailing={<FormArrow/>} onPress={(): void => {
-            openURL('https://twitter.com/enmityapp');
-          }}></FormRow>
-          <FormDivider />
-        </FormSection></>;
+      Patcher.after(UserSettingsOverview.prototype, "render", (_, args, { props: { children } }) => {
+         const index = children.findIndex(x => x.props.title === Messages["PREMIUM_SETTINGS"]);
+         const { version } = window["enmity"];
 
-      children.splice(nitroIndex, 0, enmitySection);
-    });
-  });
+         children.splice(index, 0, <>
+            <FormSection title="Enmity">
+               <FormRow
+                  label="Enmity"
+                  trailing={<FormLabel text={version} />}
+                  onPress={(): void => {
+                     openURL(`https://github.com/enmity-mod/enmity/commit/${version}`);
+                  }}
+               />
+               <FormDivider />
+               <FormRow
+                  label="Plugins"
+                  trailing={<FormArrow />}
+                  onPress={(): void => {
+                     Navigation.default.push(PluginPage, {});
+                  }}
+               />
+               <FormDivider />
+               <FormRow
+                  label="Themes"
+                  trailing={<FormArrow />}
+                  onPress={(): void => {
+                     Navigation.default.push(ThemePage, {});
+                  }}
+               />
+               <FormDivider />
+               <FormRow
+                  label={<FormLabel style={{ color: "#FFC0CB" }} text={"Reload Discord"} />}
+                  arrowShown={true}
+                  onPress={(): void => {
+                     showDialog({
+                        title: "Are you sure you want to reload Discord?",
+                        confirmText: "Yes",
+                        cancelText: "No",
+                        onConfirm: reloadDiscord,
+                     });
+                  }}
+               />
+            </FormSection>
+            <FormSection>
+               <FormRow
+                  label={<FormLabel text={"Discord"} />}
+                  trailing={<FormArrow />}
+                  onPress={(): void => {
+                     openURL("https://discord.gg/rMdzhWUaGT");
+                  }}
+               />
+               <FormDivider />
+               <FormRow
+                  label={<FormLabel text={"GitHub"} />}
+                  trailing={<FormArrow />}
+                  onPress={(): void => {
+                     openURL("https://github.com/enmity-mod/enmity");
+                  }}
+               />
+               <FormDivider />
+               <FormRow
+                  label={<FormLabel text={"Twitter"} />}
+                  trailing={<FormArrow />}
+                  onPress={(): void => {
+                     openURL("https://twitter.com/enmityapp");
+                  }}
+               />
+               <FormDivider />
+            </FormSection>
+         </>);
+      });
+   });
 }
