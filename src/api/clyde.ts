e9eb5c4edf1d41e4fad9c1f616cd67b2ec1a99ea
eternal <eternal.git@gmail.com>
2022-04-07 03:03:29 +0100

    use a more reliable icon source

--- src/api/clyde.ts
+++ src/api/clyde.ts
@@ -4,7 +4,7 @@ const clydeModule = getModule(m => m.default?.receiveMessage);
 const createBotMessageModule = getModule(m => m.createBotMessage);
 const botAvatarsModule = getModule(m => m.default?.BOT_AVATARS);
 
-botAvatarsModule.default.BOT_AVATARS.enmity = "https://cdn.discordapp.com/attachments/950850316318933004/961398357866217502/101209876.png"
+botAvatarsModule.default.BOT_AVATARS.enmity = "https://github.com/enmity-mod.png";
 
 export function sendReply(channelID: string, content: (string | object), username?: string, avatarURL?: string): void {
   let receivedMessage = createBotMessageModule.createBotMessage(channelID, '');
