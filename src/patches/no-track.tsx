654eb1d3818c5c94ebadfb1d5834e3e4fe6d88c9
eternal <eternal.git@gmail.com>
2022-04-24 05:59:06 +0100

    block over 200 trackers in total :trollface:

--- src/patches/no-track.tsx
+++ src/patches/no-track.tsx
@@ -4,22 +4,34 @@ import { create } from '../utils/patcher';
 const Patcher = create('no-track');
 
 const blacklist = [
-  'useAndTrack',
-  'TextTrack',
+  'useTrackThreadArchivalDurationUpsell',
+  'useAndTrackNonFriendDMAccept',
+  'StreamRTCAnalyticsContext',
+  'useAndTrackDMSpamAccept',
   'useAnalyticsContext',
-  'useTrackThreadArchivalDurationUpsell'
+  'trackExposure',
+  'useAndTrack',
+  'TextTrack'
 ];
 
-const Trackers = getModules(m => typeof m === 'object' && Object.keys(m)?.some(e => (~e.toLowerCase().indexOf('track') || ~e.toLowerCase().indexOf('analytics')) && !blacklist.some(b => ~e.indexOf(b))));
-const Reporters = getModules(m => typeof m === 'object' && Object.keys(m)?.some(e => ~e.toLowerCase().indexOf('crashreport') && !blacklist.some(b => ~e.indexOf(b))));
+const Trackers = getModules(m => typeof m === 'object' && [...Object.keys(m), ...Object.keys(m.__proto__), ...Object.keys(m.prototype ?? {})].some(e => (~e.toLowerCase().indexOf('track') || ~e.toLowerCase().indexOf('analytics'))));
+const Reporters = getModules(m => typeof m === 'object' && [...Object.keys(m), ...Object.keys(m.__proto__), ...Object.keys(m.prototype ?? {})].some(e => ~e.toLowerCase().indexOf('crashreport')));
 
 export default function () {
   for (let i = 0; i < Trackers.length; i++) {
-    traverse(Trackers[i], key => (~key.toLowerCase().indexOf('track') || ~key.toLowerCase().indexOf('analytics')) && !blacklist.some(b => ~key.indexOf(b)));
+    const filter = (key) => (~key.toLowerCase().indexOf('track') || ~key.toLowerCase().indexOf('analytics')) && !blacklist.some(b => key === b);
+    const tracker = Trackers[i];
+
+    traverse(tracker, filter);
+    traverse(tracker.default, filter);
   }
 
   for (let i = 0; i < Reporters.length; i++) {
-    traverse(Reporters[i], key => ~key.toLowerCase().indexOf('crashreport') && !blacklist.some(b => ~key.indexOf(b)));
+    const filter = (key) => ~key.toLowerCase().indexOf('crashreport') && !blacklist.some(b => key === b);
+    const reporter = Reporters[i];
+
+    traverse(reporter, filter);
+    traverse(reporter.default, filter);
   }
 
   const Sentry = {
@@ -50,20 +62,36 @@ export default function () {
 }
 
 function traverse(object, filter) {
-  const keys = [...Object.keys(object), ...Object.keys(object.__proto__)].filter(filter);
+  if (!object || !filter) return;
+  const keys = [
+    ...Object.keys(object),
+    ...Object.keys(object.__proto__),
+    ...Object.keys(object.prototype ?? {})
+  ].filter(filter);
 
   for (let i = 0; i < keys.length; i++) {
     const key = keys[i];
-    if (!~['function', 'object'].indexOf(typeof object[key])) {
+    const type = typeof (object[key] ?? object.__proto__[key] ?? object.prototype?.[key]);
+    if (!~['function', 'object'].indexOf(type) || blacklist.some(b => key === b)) {
       continue;
     }
 
     if (typeof object[key] === 'object') {
       traverse(object[key], filter);
     } else {
-      try {
-        Patcher.instead(object, key, () => { });
-      } catch { }
+      if (object.prototype?.[key] && isConfigurable(object.prototype, key)) {
+        Patcher.instead(object.prototype, key, () => Promise.resolve());
+      } else if (object.__proto__[key] && isConfigurable(object.__proto__, key)) {
+        Patcher.instead(object.__proto__, key, () => Promise.resolve());
+      } else if (object[key] && isConfigurable(object, key)) {
+        Patcher.instead(object, key, () => Promise.resolve());
+      }
     }
   }
 };
+
+function isConfigurable(object, key) {
+  const descriptor = Object.getOwnPropertyDescriptor(object, key);
+  if (!descriptor.configurable) return false;
+  return true;
+}
