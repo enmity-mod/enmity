d31a8178d5d9ba9fc262e6442fc6a7fb9e56e734
eternal <eternal.git@gmail.com>
2022-05-29 09:41:20 +0100

    [BREAKING CHANGE] goodbye legacy modules

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,13 @@
-import { Dialog, Native, REST, Toasts, Token, Users, Clipboard, Storage } from '@metro/common';
 import * as Plugins from '@managers/plugins';
 import * as Themes from '@managers/themes';
 import * as Components from '@components';
 import * as Commands from '@api/commands';
 import * as Settings from '@api/settings';
+import { Native } from '@metro/common';
 import * as Assets from '@api/assets';
 import * as Clyde from '@api/clyde';
 import * as Patcher from '@patcher';
 import * as Modules from '@metro';
-import React from 'react';
 
 export const API = {
   modules: Modules,
@@ -21,25 +20,7 @@ export const API = {
   settings: Settings,
   components: Components,
   native: Native,
-  assets: Assets,
-
-  // Legacy modules
-  clipboard: Clipboard,
-  storage: Storage,
-  token: Token,
-  users: Users,
-  rest: REST,
-  react: {
-    ...React,
-    ...Components,
-    React
-  },
-  dialog: {
-    showDialog: Dialog.show
-  },
-  toasts: {
-    showToast: Toasts.open
-  }
+  assets: Assets
 };
 
 export function initialize(): void {
