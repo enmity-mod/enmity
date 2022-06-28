9ef2bb7ceeab562208d95dacb3fd5dcbe8460ef3
eternal <eternal.git@gmail.com>
2022-06-28 02:21:58 +0100

    port wrapinhooks from unbound

--- src/modules/utilities/index.ts
+++ src/modules/utilities/index.ts
@@ -3,6 +3,7 @@ export { default as debounce } from './debounce';
 export { default as createStore } from './createStore';
 export { default as findInReactTree } from './findInReactTree';
 export { default as normalizeSize } from './normalizeSize';
+export { default as wrapInHooks } from './wrapInHooks';
 export { default as findInTree } from './findInTree';
 export { default as sleep } from './sleep';
 export { default as uuid } from './uuid';
---
+++ src/modules/utilities/wrapInHooks.ts
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const overrides = {
+  useMemo: factory => factory(),
+  useState: initialState => [initialState, () => void 0],
+  useReducer: initialValue => [initialValue, () => void 0],
+  useEffect: () => { },
+  useLayoutEffect: () => { },
+  useRef: () => ({ current: null }),
+  useCallback: callback => callback
+};
+
+const keys = Object.keys(overrides);
+
+export default (component) => {
+  return (...args) => {
+    const ReactDispatcher = (React as any).__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentDispatcher.current;
+    const originals = keys.map(e => [e, ReactDispatcher[e]]);
+
+    Object.assign(ReactDispatcher, overrides);
+
+    const res = {
+      rendered: null,
+      error: null
+    };
+
+    try {
+      res.rendered = component(...args);
+    } catch (error) {
+      res.error = error;
+    }
+
+    Object.assign(ReactDispatcher, Object.fromEntries(originals));
+
+    if (res.error) {
+      throw res.error;
+    }
+
+    return res.rendered;
+  };
+};
