dddba1e82c4a9e8a4e2198e6054290caee2b402d
eternal <eternal.git@gmail.com>
2022-07-15 20:09:44 +0100

    clyde: grab current channel if none is provided

--- src/api/clyde.ts
+++ src/api/clyde.ts
@@ -1,11 +1,15 @@
 import { Clyde, Messages, Avatars } from '@metro/common';
+import { getByProps } from '@metro';
+
+const Channels = getByProps('getLastSelectedChannelId');
 
 try {
   Avatars.BOT_AVATARS.ENMITY = 'https://github.com/enmity-mod.png';
 } catch { }
 
 export function sendReply(channelID: string, content: (string | object), username?: string, avatarURL?: string): void {
-  const msg = Clyde.createBotMessage(channelID, '');
+  const channel = channelID ?? Channels?.getChannelId?.();
+  const msg = Clyde.createBotMessage(channel, '');
 
   msg.author.username = username ?? 'Enmity';
   msg.author.avatar = avatarURL ? username : 'ENMITY';
@@ -20,5 +24,5 @@ export function sendReply(channelID: string, content: (string | object), usernam
     Object.assign(msg, content);
   }
 
-  Messages.receiveMessage(channelID, msg);
+  Messages.receiveMessage(channel, msg);
 }
