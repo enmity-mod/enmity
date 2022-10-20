99a56b5f06d0385ba3195a182707e084b6d31e33
eternal <eternal.git@gmail.com>
2022-10-20 23:26:58 +0100

    oops

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-async function init() {
-  // Setup asset handler early to capture most assets then initialize enmity
-  await import('@api/assets');
-
-  const Enmity = await import('@core');
-  Enmity.initialize();
-}
-
 try {
-  init();
-} catch (e) {
-  alert(`Enmity failed to initialize: ${e.message}`);
-  console.error('Enmity failed to initialize:', e.message);
+  // Setup asset handler early to capture most assets then initialize enmity
+  import('@api/assets').then(() => {
+    import('@core').then(Enmity => {
+      Enmity.initialize();
+    });
+  });
+} catch (error) {
+  alert(`Enmity failed to initialize: ${error.message}`);
+  console.error(error);
 }
+
+export { };
