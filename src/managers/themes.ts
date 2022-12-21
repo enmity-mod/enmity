e3d1e96ca789e54ce3682ce4d7730157579457df
eternal <eternal.git@gmail.com>
2022-12-21 13:23:09 +0000

    fix light theme bug

--- src/managers/themes.ts
+++ src/managers/themes.ts
@@ -1,6 +1,6 @@
 import type { Theme as ThemeType } from 'enmity/managers/themes';
+import { Theme, REST, Dispatcher } from '@metro/common';
 import { sendCommand } from '@modules/native';
-import { Theme, REST } from '@metro/common';
 import { getByProps } from '@metro';
 
 type Theme = ThemeType;
@@ -106,3 +106,16 @@ export async function uninstallTheme(name: string, callback?: (data) => void): P
     });
   });
 }
+
+const data = { current: Theme.theme };
+
+Dispatcher.subscribe('USER_SETTINGS_PROTO_UPDATE', () => {
+  if (Theme.theme === data.current) return;
+
+  try {
+    data.current = Theme.theme;
+    applyTheme(theme);
+  } catch {
+    console.error('Failed to declare appearance change.');
+  }
+});
