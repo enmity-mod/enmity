4ccff1733457bc7d6c1f931c84a520348a1ddf4c
eternal <eternal.git@gmail.com>
2023-01-03 10:07:00 +0000

    fix commands

--- src/api/commands.ts
+++ src/api/commands.ts
@@ -11,7 +11,7 @@ const [
 ] = bulk(
   filters.byProps('getBuiltInCommands'),
   filters.byProps('getApplicationIconURL'),
-  filters.byProps('useSearchManager')
+  m => m.default?.getQueryCommands && m.useDiscoveryState
 );
 
 let commands: Command[] = [];
@@ -67,89 +67,105 @@ function unregisterCommands(caller: string): void {
 function initialize() {
   Commands.BUILT_IN_SECTIONS['enmity'] = section;
 
-  Patcher.after(SearchStore.default, 'getQueryCommands', (_, [, , query], res) => {
-    if (!query || query.startsWith('/')) return;
-    res ??= [];
+  try {
+    Patcher.after(SearchStore.default, 'getQueryCommands', (_, [, , query], res) => {
+      if (!query || query.startsWith('/')) return;
+      res ??= [];
 
-    for (const command of commands) {
-      if (!~command.name?.indexOf(query) || res.some(e => e.__enmity && e.id === command.id)) {
-        continue;
+      for (const command of commands) {
+        if (!~command.name?.indexOf(query) || res.some(e => e.__enmity && e.id === command.id)) {
+          continue;
+        }
+
+        try {
+          res.unshift(command);
+        } catch {
+          // Discord calls Object.preventExtensions on the result when switching channels
+          // Therefore, re-making the result array is required.
+          res = [...res, command];
+        }
       }
+    });
+  } catch {
+    console.error('Patching getQueryCommands failed.');
+  }
 
+  try {
+    Patcher.instead(SearchStore.default, 'getApplicationSections', (_, args, orig) => {
       try {
-        res.unshift(command);
+        const res = orig.apply(self, args) ?? [];
+
+        if (!res.find(r => r.id === section.id)) {
+          res.push(section);
+        };
+
+        return res;
       } catch {
-        // Discord calls Object.preventExtensions on the result when switching channels
-        // Therefore, re-making the result array is required.
-        res = [...res, command];
+        return [];
       }
-    }
-  });
+    });
+  } catch {
+    console.error('Patching getApplicationSections failed.');
+  }
 
-  Patcher.instead(SearchStore.default, 'getApplicationSections', (_, args, orig) => {
-    try {
-      const res = orig.apply(self, args) ?? [];
+  try {
+    Patcher.after(SearchStore, 'useDiscoveryState', (_, [, type], res) => {
+      if (type !== 1) return;
 
-      if (!res.find(r => r.id === section.id)) {
-        res.push(section);
-      };
+      if (!res.sectionDescriptors?.find?.(s => s.id === section.id)) {
+        res.sectionDescriptors ??= [];
+        res.sectionDescriptors.push(section);
+      }
 
-      return res;
-    } catch {
-      return [];
-    }
-  });
+      if ((!res.filteredSectionId || res.filteredSectionId === section.id) && !res.activeSections.find(s => s.id === section.id)) {
+        res.activeSections.push(section);
+      }
 
-  Patcher.after(SearchStore, 'useSearchManager', (_, [, type], res) => {
-    if (type !== 1) return;
+      if (commands.some(c => !res.commands?.find?.(r => r.id === c.id))) {
+        res.commands ??= [];
 
-    if (!res.sectionDescriptors?.find?.(s => s.id === section.id)) {
-      res.sectionDescriptors ??= [];
-      res.sectionDescriptors.push(section);
-    }
+        // De-duplicate commands
+        const collection = [...res.commands, ...commands];
+        res.commands = [...new Set(collection).values()];
+      }
 
-    if ((!res.filteredSectionId || res.filteredSectionId === section.id) && !res.activeSections.find(s => s.id === section.id)) {
-      res.activeSections.push(section);
-    }
+      if ((!res.filteredSectionId || res.filteredSectionId === section.id) && !res.commandsByActiveSection.find(r => r.section.id === section.id)) {
+        res.commandsByActiveSection.push({
+          section: section,
+          data: commands
+        });
+      }
 
-    if (commands.some(c => !res.commands?.find?.(r => r.id === c.id))) {
-      res.commands ??= [];
+      const active = res.commandsByActiveSection.find(r => r.section.id === section.id);
+      if ((!res.filteredSectionId || res.filteredSectionId === section.id) && active && active.data.length === 0 && commands.length !== 0) {
+        active.data = commands;
+      }
 
-      // De-duplicate commands
-      const collection = [...res.commands, ...commands];
-      res.commands = [...new Set(collection).values()];
-    }
+      /*
+       * Filter out duplicate built-in sections due to a bug that causes
+       * the getApplicationSections path to add another built-in commands
+       * section to the section rail
+       */
 
-    if ((!res.filteredSectionId || res.filteredSectionId === section.id) && !res.commandsByActiveSection.find(r => r.section.id === section.id)) {
-      res.commandsByActiveSection.push({
-        section: section,
-        data: commands
-      });
-    }
+      const builtIn = res.sectionDescriptors.filter(s => s.id === '-1');
+      if (builtIn.length > 1) {
+        res.sectionDescriptors = res.sectionDescriptors.filter(s => s.id !== '-1');
+        res.sectionDescriptors.push(builtIn.find(r => r.id === '-1'));
+      }
+    });
+  } catch {
+    console.error('Patching useDiscoveryState failed.');
+  }
 
-    const active = res.commandsByActiveSection.find(r => r.section.id === section.id);
-    if ((!res.filteredSectionId || res.filteredSectionId === section.id) && active && active.data.length === 0 && commands.length !== 0) {
-      active.data = commands;
-    }
-
-    /*
-     * Filter out duplicate built-in sections due to a bug that causes
-     * the getApplicationSections path to add another built-in commands
-     * section to the section rail
-     */
-
-    const builtIn = res.sectionDescriptors.filter(s => s.id === '-1');
-    if (builtIn.length > 1) {
-      res.sectionDescriptors = res.sectionDescriptors.filter(s => s.id !== '-1');
-      res.sectionDescriptors.push(builtIn.find(r => r.id === '-1'));
-    }
-  });
-
-  Patcher.after(Assets, 'getApplicationIconURL', (_, [props], res) => {
-    if (props.id === 'enmity') {
-      return section.icon;
-    }
-  });
+  try {
+    Patcher.after(Assets, 'getApplicationIconURL', (_, [props], res) => {
+      if (props.id === 'enmity') {
+        return section.icon;
+      }
+    });
+  } catch {
+    console.error('Patching getApplicationIconURL failed.');
+  }
 }
 
 export { registerCommands, unregisterCommands };
