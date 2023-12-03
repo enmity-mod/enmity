294bf5189c310fb9c8804a50d8b49f35e24fafea
Rosie <acquitefx@gmail.com>
2023-12-03 17:56:41 +0000

    Make commands use built-in section for simplicity

--- src/api/commands.ts
+++ src/api/commands.ts
@@ -1,171 +1,64 @@
 import { ApplicationCommandInputType, ApplicationCommandType, Command } from 'enmity/api/commands';
-import { filters, bulk } from '@metro';
+import { getByProps } from '@metro';
 import { create } from '@patcher';
 
 const Patcher = create('enmity-commands');
-
-const [
-  Commands,
-  Assets,
-  SearchStore
-] = bulk(
-  filters.byProps('getBuiltInCommands'),
-  filters.byProps('getApplicationIconURL'),
-  m => m.default?.getQueryCommands && m.useDiscoveryState
-);
+const Commands = getByProps('getBuiltInCommands');
+const sectionId = 'enmity';
 
 let commands: Command[] = [];
 
-export const section = {
-  id: 'enmity',
-  type: 1,
-  name: 'Enmity',
-  icon: 'https://files.enmity.app/icon.png'
-};
-
 try {
-  initialize();
+    initialize();
 } catch (e) {
-  console.error('Failed to patch commands: ', e.message);
+    console.error('Failed to patch commands: ', e.message);
 }
 
 function registerCommands(caller: string, cmds: Command[]): void {
-  if (!caller || typeof caller !== 'string') {
-    throw new TypeError('first argument caller must be of type string');
-  } else if (!cmds || !Array.isArray(cmds)) {
-    throw new TypeError('second argument cmds must be of type array');
-  }
+    if (!caller || typeof caller !== 'string') {
+        throw new TypeError('first argument caller must be of type string');
+    } else if (!cmds || !Array.isArray(cmds)) {
+        throw new TypeError('second argument cmds must be of type array');
+    }
 
-  for (const command in cmds) {
-    const cmd = cmds[command];
-    cmds[command] = {
-      displayName: cmd.name,
-      displayDescription: cmd.description,
-      type: ApplicationCommandType.Chat,
-      inputType: ApplicationCommandInputType.BuiltIn,
-      id: `enmity-${cmd.name.replaceAll(' ', '-')}`,
-      applicationId: section.id,
-      ...cmd,
-      // @ts-ignore
-      __enmity: true,
-      caller: caller
-    };
-  }
+    for (const command in cmds) {
+        const builtInCommands = Commands.getBuiltInCommands(ApplicationCommandType.Chat, true, false);
+        builtInCommands.sort((a, b) => parseInt(b.id) - parseInt(a.id));
 
-  commands.push(...cmds);
+        const lastCommand = builtInCommands[builtInCommands.length - 1];
+        const cmd = cmds[command];
+
+        cmds[command] = {
+            displayName: cmd.name,
+            displayDescription: cmd.description,
+            type: ApplicationCommandType.Chat,
+            inputType: ApplicationCommandInputType.BuiltIn,
+            id: `${parseInt(lastCommand.id, 10) - 1}`,
+            applicationId: sectionId,
+            ...cmd,
+
+            // @ts-ignore
+            __enmity: true,
+            caller
+        };
+    }
+
+    commands.push(...cmds);
 }
 
 function unregisterCommands(caller: string): void {
-  if (!caller || typeof caller !== 'string') {
-    throw new TypeError('first argument caller must be of type string');
-  }
+    if (!caller || typeof caller !== 'string') {
+        throw new TypeError('first argument caller must be of type string');
+    }
 
-  // @ts-ignore
-  commands = commands.filter(c => c.caller !== caller);
+    // @ts-ignore
+    commands = commands.filter(c => c.caller !== caller);
 }
 
 function initialize() {
-  Commands.BUILT_IN_SECTIONS['enmity'] = section;
-
-  try {
-    Patcher.after(SearchStore.default, 'getQueryCommands', (_, [, , query], res) => {
-      if (!query || query.startsWith('/')) return;
-      res ??= [];
-
-      for (const command of commands) {
-        if (!~command.name?.indexOf(query) || res.some(e => e.__enmity && e.id === command.id)) {
-          continue;
-        }
-
-        try {
-          res.unshift(command);
-        } catch {
-          // Discord calls Object.preventExtensions on the result when switching channels
-          // Therefore, re-making the result array is required.
-          res = [...res, command];
-        }
-      }
+    Patcher.after(Commands, 'getBuiltInCommands', (_, [type], res) => {
+        if (type === ApplicationCommandType.Chat) return [...res, ...commands];
     });
-  } catch {
-    console.error('Patching getQueryCommands failed.');
-  }
-
-  try {
-    Patcher.instead(SearchStore.default, 'getApplicationSections', (_, args, orig) => {
-      try {
-        const res = orig.apply(self, args) ?? [];
-
-        if (!res.find(r => r.id === section.id)) {
-          res.push(section);
-        };
-
-        return res;
-      } catch {
-        return [];
-      }
-    });
-  } catch {
-    console.error('Patching getApplicationSections failed.');
-  }
-
-  try {
-    Patcher.after(SearchStore, 'useDiscoveryState', (_, [, type], res) => {
-      if (type !== 1) return;
-
-      if (!res.sectionDescriptors?.find?.(s => s.id === section.id)) {
-        res.sectionDescriptors ??= [];
-        res.sectionDescriptors.push(section);
-      }
-
-      if ((!res.filteredSectionId || res.filteredSectionId === section.id) && !res.activeSections.find(s => s.id === section.id)) {
-        res.activeSections.push(section);
-      }
-
-      if (commands.some(c => !res.commands?.find?.(r => r.id === c.id))) {
-        res.commands ??= [];
-
-        // De-duplicate commands
-        const collection = [...res.commands, ...commands];
-        res.commands = [...new Set(collection).values()];
-      }
-
-      if ((!res.filteredSectionId || res.filteredSectionId === section.id) && !res.commandsByActiveSection.find(r => r.section.id === section.id)) {
-        res.commandsByActiveSection.push({
-          section: section,
-          data: commands
-        });
-      }
-
-      const active = res.commandsByActiveSection.find(r => r.section.id === section.id);
-      if ((!res.filteredSectionId || res.filteredSectionId === section.id) && active && active.data.length === 0 && commands.length !== 0) {
-        active.data = commands;
-      }
-
-      /*
-       * Filter out duplicate built-in sections due to a bug that causes
-       * the getApplicationSections path to add another built-in commands
-       * section to the section rail
-       */
-
-      const builtIn = res.sectionDescriptors.filter(s => s.id === '-1');
-      if (builtIn.length > 1) {
-        res.sectionDescriptors = res.sectionDescriptors.filter(s => s.id !== '-1');
-        res.sectionDescriptors.push(builtIn.find(r => r.id === '-1'));
-      }
-    });
-  } catch {
-    console.error('Patching useDiscoveryState failed.');
-  }
-
-  try {
-    Patcher.after(Assets, 'getApplicationIconURL', (_, [props], res) => {
-      if (props.id === 'enmity') {
-        return section.icon;
-      }
-    });
-  } catch {
-    console.error('Patching getApplicationIconURL failed.');
-  }
 }
 
 export { registerCommands, unregisterCommands };
