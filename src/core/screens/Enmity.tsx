353bc47486847ba7320df2f8ed5ba4baaff19c90
byron <47872200+byeoon@users.noreply.github.com>
2024-03-15 16:49:36 +0000

    Final sort before PR

--- src/core/screens/Enmity.tsx
+++ src/core/screens/Enmity.tsx
@@ -46,7 +46,7 @@ export default function Page({ settings }) {
   // @ts-ignore
   const Runtime = HermesInternal.getRuntimeProperties();
   const Plugins = getPlugins().map(p => p.name).sort();
-  const Themes = listThemes().map(t => t.name);
+  const Themes = listThemes().map(t => t.name).sort();
 
   return <ScrollView>
     <KeyboardAvoidingView
